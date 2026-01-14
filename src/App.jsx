9268d251a92884c7622cd5d464a5e7eddc93eef6
AkhsanDaffa <akhsandaffa01@gmail.com>
2026-01-14 14:32:08 +0700

    Change Title

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Webhook With Smee.io V2</h1>
+      <h1>Hello Jenkins CI/CD Sukses!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
